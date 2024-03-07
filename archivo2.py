4f45ae3f00bd50205259e8eb5b5313a1bc2e78d2
Sidorc <60902722+Sidorc@users.noreply.github.com>
2024-03-07 10:28:55 -0500

    Mas cosas para el ensayo de conflictos

--- archivo2.py
+++ archivo2.py
@@ -2,6 +2,8 @@ print(__name__)
 
 if __name__ == "__main__":
     print(f"Ejecutando desde archivo principal con __name__={__name__}")
+elif __name__ == "otra cosa":
+    print("Otra cosa dada")
 else:
     print("Ejecuntando desde archivo importado")
 
