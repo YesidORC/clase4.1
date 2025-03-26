1e72ad73a1f4629d82392616e66377738b7c3d68
YesidORC <114194820+YesidORC@users.noreply.github.com>
2025-03-26 12:09:54 -0500

    Modificaciones para ejemplificar mejor

--- archivo2.py
+++ archivo2.py
@@ -1,12 +1,29 @@
-print(__name__)
+class Paciente():
+    def __init__(self):
+        self.__nombre = '' 
+        self.__cedula = 0 
+        self.__genero = '' 
+        self.__servicio = '' 
+
+class Sistema:
+    def __init__(self):
+        self.__lista_pacientes = [] 
+    def __str__(self):
+        return f" {len(self.__lista_pacientes} en el sistema "
+    def ingresarPaciente(self,pac):
+        self.__lista_pacientes.append(pac) 
 
 if __name__ == "__main__":
-    print(f"Ejecutando desde archivo principal con __name__={__name__}")
+    print(f"Ejecutando desde archivo principal con __name__= {__name__}  /
+    se crean objetos de para hacer pruebas Paciente y Sistema")
+    p =  Paciente()
+    hosp =  Sistema()
+    hosp.ingresarPaciente(p)
+    print(hosp)
 elif __name__ == "otra cosa":
     print("Otra cosa dada")
 else:
     print("Ejecutando desde archivo importado_ voy a verifificar conflictos")
-    print("Ejecutando desde archivo importado_ voy a verifificar conflictos")
+    
 
 
-print(f"buscando mas {2*3*5*6} cosas conflictivas")
